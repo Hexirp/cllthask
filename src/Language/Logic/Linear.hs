6f5b38590e5e6318343dca98b8aa82ac6e8beea1
Hexirp <Hexirp@users.noreply.github.com>
2017-10-13 16:45:23 +0900

    Update Language.Logic.Linear to add syntax

--- src/Language/Logic/Linear.hs
+++ src/Language/Logic/Linear.hs
@@ -1,2 +1,17 @@
 module Language.Logic.Linear where
  import Prelude
+
+ data Seq = Seq [Type] [Type]
+
+ data Type
+  = Time Type Type
+  | Tiu
+  | Para Type Type
+  | Pau
+  | Plus Type Type
+  | Plu
+  | With Type Type
+  | Wiu
+  | Ofco Type
+  | Whno Type
+  | Nega Type

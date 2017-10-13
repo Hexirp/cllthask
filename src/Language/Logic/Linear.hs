7dbc6424743364d1847452fd8ad0af83d6b29731
Hexirp <Hexirp@users.noreply.github.com>
2017-10-13 17:07:24 +0900

    Add 'init' and 'cut'

--- src/Language/Logic/Linear.hs
+++ src/Language/Logic/Linear.hs
@@ -1,7 +1,7 @@
 module Language.Logic.Linear where
  import Prelude
 
- data Seq = Seq [Type] [Type]
+ data Seq = Seq [Type] [Type] deriving Eq
 
  data Type
   = Time Type Type
@@ -15,3 +15,13 @@ module Language.Logic.Linear where
   | Ofco Type
   | Whno Type
   | Nega Type
+  deriving Eq
+
+ init :: Type -> Seq
+ init a = Seq [a] [a]
+
+ cut :: Seq -> Seq -> Maybe Seq
+ cut (Seq g0 (a : s0)) (Seq (b : g1) s1) = case a == b of
+  False -> Nothing
+  True -> Just $ Seq (g0 ++ g1) (s0 ++ s1)
+ cut _ _ = Nothing

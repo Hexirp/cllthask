900e2feeb778f16522dd47bfaa61284c8b0c33ba
Hexirp <Hexirp@users.noreply.github.com>
2017-12-25 11:25:32 +0900

    Update for practice

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,23 @@
 module Main where
+ import Prelude
 
-import Lib
+ main :: IO ()
+ main = getLine >>= print . foldMap parse
 
-main :: IO ()
-main = someFunc
+ parse :: Char -> Expr
+ parse '(' = Expr 0 1
+ parse ')' = Expr 1 0
+ parse _   = Expr 0 0
+
+ data Expr = Expr Int Int
+
+ instance Monoid Expr where
+  mempty = Expr 0 0
+  mappend (Expr m n) (Expr o p) = case compare n o of
+   LT -> Expr (o - n + m) p
+   EQ -> Expr m p
+   GT -> Expr m (n - o + p)
+
+ instance Show Expr where
+  show (Expr 0 0) = "Yes"
+  show _          = "No"

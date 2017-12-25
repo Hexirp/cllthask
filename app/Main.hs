03a9274e3085dce57b1c2a6f8dee532568c364e9
Hexirp <Hexirp@users.noreply.github.com>
2017-12-25 12:20:06 +0900

    Update for Forth

--- app/Main.hs
+++ app/Main.hs
@@ -2,31 +2,15 @@ module Main where
  import Prelude
 
  main :: IO ()
- main = getLine >>= print . foldMap parse
+ main = main0 emptySt
 
- parse :: Char -> Expr
- parse 't' = Expr T T
- parse 'r' = Expr R R
- parse 'u' = Expr U U
- parse 'e' = Expr E E
- parse _   = Other
+ main0 :: St -> IO ()
+ main0 st = getLine >>= next . foldr run st . words
 
- data TRUE = T | R | U | E
+ next :: St -> IO ()
+ next st = show st >> main0 st
 
- data Expr = Empty | Other | Expr TRUE TRUE
-
- instance Monoid Expr where
-  mempty = Empty
-  mappend Empty a = a
-  mappend Other _ = Other
-  mappend (Expr m n) (Expr o p) = case (n, o) of
-   (T, R) -> Expr m p
-   (R, U) -> Expr m p
-   (U, E) -> Expr m p
-   _      -> Other
-  mappend _ Other = Other
-  mappend a Empty = a
-
- instance Show Expr where
-  show (Expr T E) = "Yes"
-  show _    = "No"
+ run :: String -> St -> St
+ run w st = case search w st of
+  Nothing -> failureSt
+  Just f -> f St

be48ed5a4f034ea48f5a734943c87118dfaec65e
Hexirp <Hexirp@users.noreply.github.com>
2017-12-26 14:13:59 +0900

    Update for pratice

--- app/Main.hs
+++ app/Main.hs
@@ -1,22 +1,32 @@
 module Main where
  import Prelude
 
+ import Data.Proxy (Proxy(Proxy))
+
  main :: IO ()
- main = getLine >>= putStrLn . check . foldMap parse
+ main = getLine >>= print . parse (Proxy :: Proxy Expr)
 
  data Expr = Expr String
-
- check :: Expr -> String
- check (Expr "true") = "OK!"
- check _             = "NG!"
-
- parse :: Char -> Expr
- parse = Expr . singleton
 
  instance Monoid Expr where
   mempty = Expr []
 
   mappend (Expr a) (Expr b) = Expr (mappend a b)
+
+ instance Parser Expr where
+  trans = Expr . singleton
+
+  check (Expr "true") = True
+  check _             = False
+
+ class Monoid a => Parser a where
+  trans :: Char -> a
+  check :: a -> Bool
+
+  parse :: Proxy a -> String -> Bool
+  parse p = check . is p . foldMap trans where
+   is :: Proxy a -> a -> a
+   is _ = id
 
  singleton :: a -> [a]
  singleton a = [a]

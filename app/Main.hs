5dd3b969ee05a5513a6f21ea747d0a7bcff20bf6
Hexirp <Hexirp@users.noreply.github.com>
2018-08-01 16:52:07 +0900

    Fix: インポート忘れ

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,10 @@
 module Main where
  import Prelude
 
+ import Control.Monad (unless)
+ 
+ import System.IO (hFlush, stdout)
+
  main :: IO ()
  main = do
   input <- putStr "REPL> " >> hFlush stdout >> getLine

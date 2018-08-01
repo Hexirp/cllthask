9aac9712e6c25928ca431a353a3003ff8d2cb2a5
Hexirp <Hexirp@users.noreply.github.com>
2018-08-01 17:26:06 +0900

    Add: プリンターを。パーサーはできてない
    プリンターに無限ループのバグあり

---
+++ src/Parser.hs
@@ -0,0 +1,30 @@
+module Parser where
+ import Prelude
+
+ import Control.Monad (join)
+
+ type Tree = Func
+
+ data Func = Func String Args
+
+ type Args = [Tree]
+
+ ppTree :: Tree -> String
+ ppTree = ppFunc
+
+ ppFunc :: Func -> String
+ ppFunc = unlines . ppFunc'
+
+ ppFunc' :: Func -> [String]
+ ppFunc' (Func func args) = func : ppArgs' args
+
+ ppArgs :: Args -> String
+ ppArgs = unlines . ppArgs''
+
+ ppArgs' :: Args -> [String]
+ ppArgs' = enchant . ppArgs' where
+  enchant [] = []
+  enchant (x : xs) = (',' : ' ' : x) : fmap (\x -> ' ' : ' ' : x) xs
+
+ ppArgs'' :: Args -> [String]
+ ppArgs'' = join . fmap ppFunc'

18f07b9ca773d102cd1aca9f2ccd7585e604a31c
Hexirp <Hexirp@users.noreply.github.com>
2018-11-21 22:47:58 +0900

    Fix: どこが間違っているのか確かめる

--- src/Lib.hs
+++ src/Lib.hs
@@ -18,7 +18,7 @@ module Lib
  wrapOnce a = UnsafeWrapOnce $ wrap a
 
  wrap :: a -> IO a
- wrap a = join $ wrap2 a <$> newTVarIO False
+ wrap a = join $ wrap2 a <$> (newTVarIO False :: IO (TVar Bool))
 
  wrap1 :: a -> TVar Bool -> IO a
  wrap1 a ref = atomically $ wrap2 a ref

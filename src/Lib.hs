a2b10c355f51469bbd4d658becaaa441329a9470
Hexirp <Hexirp@users.noreply.github.com>
2019-01-06 13:46:18 +0900

    Update: Tree を完成させる
    Symmetric linearly distributive category の全操作はあるはず。

--- src/Lib.hs
+++ src/Lib.hs
@@ -12,13 +12,19 @@ module Lib where
    | I Type
    | AssocTL Type Type Type -- (a * b) * c |- a * (b * c)
    | AssocTR Type Type Type -- a * (b * c) |- (a * b) * c
+   | SymmetT Type Type      -- a * b |- b * a
    | UnitTLL Type           -- 1 * a |- a
    | UnitTLR Type           -- a |- 1 * a
    | UnitTRL Type           -- a * 1 |- a
    | UnitTRR Type           -- a |- a * 1
    | AssocPL Type Type Type -- (a + b) + c |- ...
    | AssocPR Type Type Type
+   | SymmetP Type Type
    | UnitPLL Type
    | UnitPLR Type
    | UnitPRL Type
    | UnitPRR Type
+   | MappedT Tree Tree      -- (a |- b), (c |- d) ===> (a * c |- b * d)
+   | MappedP Tree Tree      -- ...
+   | DistriL Type Type Type -- a * (b + c) |- (a * b) + c
+   | DistriR Type Type Type -- ...

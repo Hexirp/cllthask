72188e8fd397f8f966b1c0b12d2b509480714caf
Hexirp <Hexirp@users.noreply.github.com>
2019-01-06 11:23:12 +0900

    Clean: 改行して 80 字に収める

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,4 +2,5 @@ module Lib where
 
  import Prelude
 
- data Type = Atom String | Neg Type | Times Type Type | Par Type Type | One | Bottom
+ data Type =
+  Atom String | Neg Type | Times Type Type | Par Type Type | One | Bottom

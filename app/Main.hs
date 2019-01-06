92b4978a4190d2a3afbdc088e3c189737285fdf6
Hexirp <Hexirp@users.noreply.github.com>
2019-01-06 09:45:26 +0900

    Update: 古いものを全て消す
    全て壊すんだ？ 心機一転する。

--- app/Main.hs
+++ app/Main.hs
@@ -2,13 +2,5 @@ module Main where
 
  import Prelude
 
- import Lib
-
  main :: IO ()
- main = do
-  a <- wrapOnce 1
-
-  a1 <- unsafeUnwrapOnce a
-  print a1
-  a2 <- unsafeUnwrapOnce a
-  print a2
+ main = return ()
--- src/Control/Arrow/Linear.hs
+++
@@ -1,7 +0,0 @@
-{-# LANGUAGE KindSignatures #-}
-
-module Control.Arrow.Linear where
-
- import Prelude hiding (id, (.))
-
- import Control.Category
